7774d1308383ca78837824d5834171612d7e3f4d
maxime <maxime.siegl@laplateforme.io>
2020-07-01 16:35:23 +0200

    pages membre check + pages modo sans signalement

--- include/php_last-message.php
+++ include/php_last-message.php
@@ -4,10 +4,11 @@
     {
         $id_posteur = $_GET['id_posteur'];
 
-        $mess_info = "SELECT * FROM messages WHERE id = '$id_posteur' AND date = 'PLUS RECENT' ";
+        $mess_info = "SELECT * FROM messages WHERE id_utilisateur = '$id_posteur' ORDER BY date DESC";
         $mess_info_query = mysqli_query($bdd, $mess_info);
         $mess = mysqli_fetch_all($mess_info_query, MYSQLI_ASSOC);
 
-        var_dump($mess);
+        //var_dump($mess);
+        //echo $mess[0]['message']; // recup le message tout en haut du tab qui est le dernier message posté
     }
 ?>
