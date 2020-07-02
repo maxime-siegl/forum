b4813932ac47e6e645838eb9ee0bf81147fe361f
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-07-02 10:11:06 +0200

    envoie bien l'id de l'utilisateur qui a écrit le message

--- include/php_interaction.php
+++ include/php_interaction.php
@@ -14,14 +14,14 @@
             $query_id_conv = mysqli_query($connexionbdd, $requete_id_conv);
             $resultat_id_conv = mysqli_fetch_all($query_id_conv, MYSQLI_ASSOC);       
             $id_conv = $resultat_id_conv[0]["id_conversation"];  
-            $id_utilisateur = $resultat_id_conv[0]["id_utilisateur"];
+            echo $id_utilisateur = $resultat_id_conv[0]["id_utilisateur"];
         
            
             //REQUETE MAJ TABLE SIGNALEMENT
             //FAIRE EN SORTE QUE L'UTILISATEUR NE SIGNAL QU'UNE FOIS            
-            $requete_signalement = "INSERT INTO interactions (id_utilisateur, id_message, signalement) VALUES ($id_user, $id_msg_report, 1)";
+            $requete_signalement = "INSERT INTO interactions (id_utilisateur, id_message, signalement) VALUES ($id_utilisateur, $id_msg_report, 1)";
             $update_signalement = mysqli_query($connexionbdd, $requete_signalement);
-            header("location:../messages.php?id_conv=$id_conv");           
+            //header("location:../messages.php?id_conv=$id_conv");           
         }
     
     if(isset($_GET["id_like"]))
