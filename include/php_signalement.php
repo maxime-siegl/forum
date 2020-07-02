e67ff46f55f29ac831d3b5c3efdb323640fa1054
maxime <maxime.siegl@laplateforme.io>
2020-07-02 11:46:08 +0200

    Gestion de signalement ok

--- include/php_signalement.php
+++ include/php_signalement.php
@@ -2,10 +2,37 @@
     if (isset($_SESSION['id_confidentialite']) && $_SESSION['id_confidentialite'] == 3 || $_SESSION['id_confidentialite'] == 4)
     {
         // recup les messages qui ont été signalé
-        $signalement = "SELECT * FROM interactions INNER JOIN utilisateurs ON interactions.id_utilisateur = utilisateurs.id INNER JOIN messages ON interactions.id_message = messages.id WHERE signalement >= 1";
+        $signalement = "SELECT login, date, message, id_message FROM signalements 
+                        INNER JOIN utilisateurs ON signalements.id_utilisateur = utilisateurs.id 
+                        INNER JOIN messages ON signalements.id_message = messages.id 
+                        GROUP BY id_message"; // recup ... en fusionnant où id_message est =
         $signalement_query = mysqli_query($bdd, $signalement);
         $msg_signal = mysqli_fetch_all($signalement_query, MYSQLI_ASSOC);
 
-        var_dump($msg_signal);
+        //var_dump($msg_signal);
+
+        foreach ($msg_signal as $info => $info_msg)
+        {
+            echo '<tr>';
+            echo '<td>'. $info_msg['login'] .'</td>';
+            echo '<td>'. $info_msg['date'] .'</td>';
+            echo '<td>'. $info_msg['message'] .'</td>';
+            echo '<td>'.'<a href="moderation.php?id_msg='.$info_msg['id_message'].'"><img src="https://img.icons8.com/ios/40/000000/delete-message.png"/></a>'.'</td>';
+            echo '<td>'.'<a href="moderation.php?id_alerte='.$info_msg['id_message'].'"><img src="https://img.icons8.com/wired/40/000000/no-reminders.png"/></a>'.'</td>';
+            echo '</tr>';
+        }
+
+        if (isset($_GET['id_alerte']) && !empty($_GET['id_alerte'])) // traitement de l'alerte
+        {
+            $id_alerte = $_GET['id_alerte'];
+            $supalerte = "DELETE FROM signalements WHERE id_message = '$id_alerte' ";
+            $supalerte_query = mysqli_query($bdd, $supalerte); 
+        }
+        else if (isset($_GET['id_msg']) && !empty($_GET['id_msg'])) // traitement du msg
+        {
+            $id_msg = $_GET['id_msg'];
+            $supmsg = "DELETE FROM messages WHERE id = '$id_msg' ";
+            $supmsg_query = mysqli_query($bdd, $supmsg);
+        }
     }
 ?>
--- moderation.php
+++ moderation.php
@@ -14,10 +14,10 @@
 </head>
 <body>
     <header>
-        <?php include('include/header.php'); ?>
+        <?php include 'include/header.php' ; ?>
     </header>
     <main>
-        <?php include('include/php_moderation.php'); ?>
+        <?php include 'include/php_moderation.php' ; ?>
         <section id="tableau_users">
             <table>
                 <thead>
@@ -28,7 +28,7 @@
                         <th>Rang</th>
                     </tr>
                 </thead>
-                <?php include 'include/php_tableau_users.php'; ?>
+                <?php include 'include/php_tableau_users.php' ; ?>
             </table>
         </section>
         <section id="msg_douteux">
@@ -37,16 +37,9 @@
                     <th>Login</th>
                     <th>Date</th>
                     <th>Message</th>
-                    <th>Signalé par</th>
                 </thead>
                 <tbody>
-                    <?php include('include/php_signalement.php'); ?>
-                    <tr>
-                        <td><?php echo $msg_signal[0]['login'] ; ?></td>
-                        <td><?php echo $msg_signal[0]['date'] ; ?></td>
-                        <td><?php echo $msg_signal[0]['message'] ; ?></td>
-                        <td><?php echo $msg_signal[0][''] ; ?></td>
-                    </tr>
+                    <?php include 'include/php_signalement.php' ; ?>
                 </tbody>
             </table>
         </section>
